0a623ec1553964aaf1a4ad2ab59ba07b3217b35a
aashutoshVITB <aashutosh.25bai11135@vitbhopal.ac.in>
2025-11-25 00:17:38 +0530

    Delete Project directory

--- Project/datastore.py
+++
@@ -1,8 +0,0 @@
-employees = [{"name": "Krishna","age": "54","department": "Technical","salary": 80600}
-             ,{"name": "Parth","age": "40","department": "Technical","salary": 150000}
-             ,{"name": "Prashant","age": "38","department": "Finance","salary": 75400}
-             ,{"name": "Bharat","age": "50","department": "Finance","salary": 108000}
-             ,{"name": "Ravi","age": "31","department": "Cleaning","salary": 15400}
-             ]
-departments = ["Technical","Finance","Cleaning"]
-tasks = []
